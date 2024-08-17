d489e81e4bb24981596e09832bd36f152d3897a3
moiz <moiz@email.com>
2024-08-17 15:01:58 +0100

    static tasks

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter, input } from '@angular/core';
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+@Input({required:true}) user! : User
+@Output() select = new EventEmitter<string>();
+
+ get imagePath (){
+  return 'assets/users/' + this.user.avatar;
+ }
+
+  onSelectUser(){   
+   this.select.emit(this.user.id)
+  }
+}
